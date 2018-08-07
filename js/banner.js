cf0b961654546323c2249f48b2ebda32c61e2530
Rodrigo Matos <rodrigo.matos@armateus.com.br>
2018-08-06 23:19:40 -0300

    atualizar

--- js/banner.js
+++ js/banner.js
@@ -1,17 +1,57 @@
-var Banner = (function(imgMini, img){
-  
-var count = 0;
+var CriadorDeBanner = (function(){
 
-    
-    
-    
-  return {
-      count: function() {
-        return count;
-      }
+    var $ = (seletor) => document.querySelector(seletor)
 
-    , increment: function() {
-        return count += 1;
-      }
-  };
+    var limparEstilos = function(objEstilos) {
+        for(var seletor in objEstilos){
+            if(objEstilos.hasOwnProperty(seletor)) {
+                $(seletor).style = objEstilos[seletor]
+            }
+        }
+    }
+
+  return function(params) {
+
+        var template = `
+            <div class="banner-hover-area">
+              <div class="banner-hover-content">
+                <div class="banner-hover-img">
+                  <img id="img-banner" src="${params.imgMenor}" alt="" />
+                   <img id="close-banner" src="image/close.png" alt=""/>
+                </div>
+              </div>
+            </div>
+        `
+
+        var elementoRaiz = document.createElement('div')
+        elementoRaiz.innerHTML = template
+
+        document.body.appendChild(elementoRaiz)
+
+        const estilosAntigos = {
+            '.banner-hover-area': $('.banner-hover-area').style,
+            '#img-banner': $('#img-banner').style,
+            '#close-banner': $('#close-banner').style,
+        }
+
+        var srcAntigo = $('#img-banner').src
+
+        var onHover = function(){
+            $('.banner-hover-area').style = 'width:100%'
+            $('#img-banner').src  = params.imgMaior
+            $('#img-banner').style = 'margin-right: 0;'
+            $('#close-banner').style = "display:flex;"
+        }
+
+        $('#close-banner').onclick = function() {
+            $('.banner-hover-area').onmouseover = null;
+            limparEstilos(estilosAntigos)
+            $('#img-banner').src = srcAntigo;
+
+             setTimeout(() => $('.banner-hover-area').onmouseover = onHover, 1000)
+        }
+
+        $('.banner-hover-area').onmouseover = onHover
+  }
+
 })();
--- js/main.js
+++ js/main.js
@@ -1,38 +1,4 @@
-//import Banner from './banner';
-
-//var styleAntigoareaHover  = null; 
-
-var areaHover = document.querySelector('.banner-hover-area');
-var image = document.querySelector('#img-banner');    
-var close = document.querySelector('#close-banner');
-
-var srcAntigo = image.src;
-var styleImageAntigo  = image.style;
-var closeImageAntigo  = close.style;
-var areaHoverStyleAntigo = areaHover.style;
-
-console.log(areaHover)
-
-atribuirNovoHover();
-
-close.onclick = function(){
-
-   areaHover.onmouseover = null;
-   areaHover.style = areaHoverStyleAntigo;
-   image.src = srcAntigo;
-   image.style = styleImageAntigo;
-   close.style = closeImageAntigo;
-    
-    setTimeout(atribuirNovoHover, 1000);
-}
-
-function atribuirNovoHover(){
-    
-       areaHover.onmouseover = function(){
-
-       areaHover.style = 'width:100%';
-       image.src  = "./image/billboard_pucrs.gif";
-       image.style = 'margin-right: 0;'
-       close.style = "display:flex;"
-    };
-}
+CriadorDeBanner({
+    imgMenor: 'http://akamai.smartadserver.com/diff/2447/6983222/aba-70x250(0).jpg',
+    imgMaior: './image/billboard_pucrs.gif'
+})
